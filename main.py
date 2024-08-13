3f90e7e80b007ddb84316d2f4a7395ca907c1ac4
Omkar Darves <omkardarves@gmail.com>
2024-08-13 17:55:23 +0530

    root path changes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # main.py
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from typing import List
 from pymongo import ReturnDocument
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="Fast Mongo")
 
@@ -10,17 +11,22 @@ from database import collection_name
 from schema import list_serial, single_serial
 from bson import ObjectId
 
-@app.get("/", response_model=List[TodoInDB])
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    swagger_url = str(request.base_url) + "docs"
+    return f'<a href="{swagger_url}">Swagger UI</a>'
+
+@app.get("/todo/", response_model=List[TodoInDB])
 async def get_todos():
     todos = collection_name.find()
     return list_serial(todos)
 
-@app.post("/", response_model=TodoInDB)
+@app.post("/todo/", response_model=TodoInDB)
 async def post_todos(todo: Todo):
-    result = collection_name.insert_one(todo.dict())
-    return {"id": str(result.inserted_id), **todo.dict()}
+    result = collection_name.insert_one(todo.model_dump())
+    return {"id": str(result.inserted_id), **todo.model_dump()}
 
-@app.put("/{id}", response_model=TodoInDB)
+@app.put("/todo/{id}", response_model=TodoInDB)
 async def put_todos(id: str, todo: Todo):
     if not ObjectId.is_valid(id):
         raise HTTPException(status_code=400, detail="Invalid ID format")
@@ -33,7 +39,7 @@ async def put_todos(id: str, todo: Todo):
         raise HTTPException(status_code=404, detail="Todo not found")
     return single_serial(result)
 
-@app.delete("/{id}", response_model=TodoInDB)
+@app.delete("/todo/{id}", response_model=TodoInDB)
 async def delete_todos(id: str):
     if not ObjectId.is_valid(id):
         raise HTTPException(status_code=400, detail="Invalid ID format")
